d42d63af5fd7294ff06c631707654563aa888f34
sarathrsk03 <sarathrajansk@gmail.com>
2024-03-10 20:46:19 +0530

    added cors2

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ from datetime import date
 from promptToDocx import letterGeneration,createPdf
 import os 
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http:localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class letterData(BaseModel):
     UUID:int
